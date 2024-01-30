7a51cda4585584ce89de1d2bcf92268ec3e75f30
Hannah Bradley <hannahthesailor@yahoo.com>
2024-01-30 00:36:27 -0500

    phase-1-functions-lab.md

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    const hqStreet = 42;
+    return Math.abs(blocks - hqStreet);
+  }
+
+  function distanceFromHqInBlocks(blocks) {
+    const hqStreet = 42;
+    return Math.abs(blocks - hqStreet);
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const distance = Math.abs(destination - start);
+    return distance * 264;
+  }
+  function distanceFromHqInFeet(blocks) {
+    const feetPerBlock = 264;
+    const distanceInBlocks = distanceFromHqInBlocks(blocks);
+    return distanceInBlocks * feetPerBlock;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    let fare;
+  
+    if (distanceInFeet <= 400) {
+        fare = 0;
+      } else if (distanceInFeet <= 2000) {
+        fare = (distanceInFeet - 400) * 0.02;
+      } else if (distanceInFeet <= 2500) {
+        fare = 25;
+      } else {
+        return 'cannot travel that far';
+      }
+    
+      return fare;
+    };
